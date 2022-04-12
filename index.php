318fdb91862f7f8851e13f7b575bbf97377c995f
SNVZero <simakov_nikita01@mail.ru>
2022-04-12 20:15:06 +0300

    Aut

--- index.php
+++ index.php
@@ -5,17 +5,13 @@ $passsword = 'letmein';
 if( isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER['PHP_AUTH_PW']))
 {
     if($_SERVER['PHP_AUTH_USER'] === $username && $_SERVER['PHP_AUTH_PW'] === $passsword )
-     {
         echo "Регистарция прошла успешно";
-    }
-    else
-    {
-        die("Неверная комбинация имя пользователя - пароль");
-    }
-    else
-    {
-        header('WWW-Autheticate: Basic realm = "Restricated Area"');
-        header('HTTP/1.0 401 Unauthorized');
-        die("Пожалуйст, введите имя пользователя и пароль");
-    }
+    else  die("Неверная комбинация имя пользователя - пароль");
+
+}
+else
+{
+    header('WWW-Autheticate: Basic realm = "Restricated Area"');
+    header('HTTP/1.0 401 Unauthorized');
+    die("Пожалуйст, введите имя пользователя и пароль");
 }
